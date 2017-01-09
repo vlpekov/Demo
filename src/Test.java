3dabcb0df7720cf108e8fdc73608ecb8bea36aa5
vlpekov <vladdey@gmail.com>
2017-01-09 11:48:20 +0200

    sds
    ghj

--- src/Test.java
+++ src/Test.java
@@ -3,28 +3,28 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		// Даден ви е масива int[] array. Създайте нов масив, с елементите от
-		// първия, но без числото 6
+		// Г„Г Г¤ГҐГ­ ГўГЁ ГҐ Г¬Г Г±ГЁГўГ  int[] array. Г‘ГєГ§Г¤Г Г©ГІГҐ Г­Г®Гў Г¬Г Г±ГЁГў, Г± ГҐГ«ГҐГ¬ГҐГ­ГІГЁГІГҐ Г®ГІ
+		// ГЇГєГ°ГўГЁГї, Г­Г® ГЎГҐГ§ Г·ГЁГ±Г«Г®ГІГ® 6
 		int lenghtArray = 0;
 		int[] array = { 6, 4, 5, 5, 6, 4, 3, 4, 7, 6 };
-		// Цикъл for, чрез който се определя дължината на новия масив.
+		// Г–ГЁГЄГєГ« for, Г·Г°ГҐГ§ ГЄГ®Г©ГІГ® Г±ГҐ Г®ГЇГ°ГҐГ¤ГҐГ«Гї Г¤ГєГ«Г¦ГЁГ­Г ГІГ  Г­Г  Г­Г®ГўГЁГї Г¬Г Г±ГЁГў.
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] != 6) {
 				lenghtArray++;
 			}
 		}
-		// Създаване на новия цикъл
+		// Г‘ГєГ§Г¤Г ГўГ Г­ГҐ Г­Г  Г­Г®ГўГЁГї Г¶ГЁГЄГєГ«
 		int[] array2 = new int[lenghtArray];
 		int j = 0;
-		// Цикъл for, чрез който се присвояват стойностите различни от 6.
+		// Г–ГЁГЄГєГ« for, Г·Г°ГҐГ§ ГЄГ®Г©ГІГ® Г±ГҐ ГЇГ°ГЁГ±ГўГ®ГїГўГ ГІ Г±ГІГ®Г©Г­Г®Г±ГІГЁГІГҐ Г°Г Г§Г«ГЁГ·Г­ГЁ Г®ГІ 6.
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] != 6) {
 				array2[j] = array[i];
 				j++;
 			}
 		}
-		System.out.println("Начален масив: " + Arrays.toString(array));
-		System.out.println("Масив без числото 6: " + Arrays.toString(array2));
+		System.out.println("ГЌГ Г·Г Г«ГҐГ­ Г¬Г Г±ГЁГў: " + Arrays.toString(array));
+		System.out.println("ГЊГ Г±ГЁГў ГЎГҐГ§ Г·ГЁГ±Г«Г®ГІГ® 66: " + Arrays.toString(array2));
 	}
 
 }
